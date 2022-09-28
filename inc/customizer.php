b5a6ab2b8d03b50451ce0316b9288df8e6c1e75c
Callum <callum@creativo-solutions.com>
2022-09-28 10:22:13 +0000

    first commit

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Creativo-22 Theme Customizer
+ *
+ * @package Creativo-22
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function creativo_theme_22_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'creativo_theme_22_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'creativo_theme_22_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'creativo_theme_22_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function creativo_theme_22_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function creativo_theme_22_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function creativo_theme_22_customize_preview_js() {
+	wp_enqueue_script( 'creativo-theme-22-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'creativo_theme_22_customize_preview_js' );
---
+++ inc/jetpack.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Creativo-22
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
+ */
+function creativo_theme_22_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support(
+		'infinite-scroll',
+		array(
+			'container' => 'main',
+			'render'    => 'creativo_theme_22_infinite_scroll_render',
+			'footer'    => 'page',
+		)
+	);
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support(
+		'jetpack-content-options',
+		array(
+			'post-details' => array(
+				'stylesheet' => 'creativo-theme-22-style',
+				'date'       => '.posted-on',
+				'categories' => '.cat-links',
+				'tags'       => '.tags-links',
+				'author'     => '.byline',
+				'comment'    => '.comments-link',
+			),
+			'featured-images' => array(
+				'archive' => true,
+				'post'    => true,
+				'page'    => true,
+			),
+		)
+	);
+}
+add_action( 'after_setup_theme', 'creativo_theme_22_jetpack_setup' );
+
+if ( ! function_exists( 'creativo_theme_22_infinite_scroll_render' ) ) :
+	/**
+	 * Custom render function for Infinite Scroll.
+	 */
+	function creativo_theme_22_infinite_scroll_render() {
+		while ( have_posts() ) {
+			the_post();
+			if ( is_search() ) :
+				get_template_part( 'template-parts/content', 'search' );
+			else :
+				get_template_part( 'template-parts/content', get_post_type() );
+			endif;
+		}
+	}
+endif;
